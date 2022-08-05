6d93225b81f9eaf7179e6f0e6d4c05dfbb307342
dianabasilio <dianabasilio99@gmail.com>
2022-08-05 13:42:50 -0500

    Added person component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
       <h1> Hi I am react app</h1>
+      <Person/>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,9 @@
-//component is a function
-function Person(){
-    return <h1>Person</h1>
+import React from 'react'
+
+const Person = () =>{
+    //this jsx sintax is transform to react create element, to call this method we need
+    //to import react
+    return <p>I'm a person</p>
 }
+
+export default Person;
