5d83f51cfaab0b3b7cf568d126ac747033ee6d53
dianabasilio <dianabasilio99@gmail.com>
2022-08-14 17:49:28 -0500

    Old way of manipulating the State.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import "./App.css";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-function App() {
-  const state = {
+class App extends Component {
+  state = {
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 70 },
       { name: "Soph", age: 17 },
     ],
+    otherState: "some other value",
   };
 
-  const switchNameHandler = () =>{
+  switchNameHandler = () => {
     //Do not do this!!! : state.persons[0].name = 'Maximun';
-  }
+    this.setState({
+      persons: [
+        { name: "Maximiliano", age: 28 },
+        { name: "Manuel", age: 20 },
+        { name: "Sophie", age: 17 },
+      ],
+    });
+  };
 
-  return (
-    <div className="App">
-      <h1> Hi I am react app</h1>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person name={state.persons[0].name} age={state.persons[0].age} />
-      <Person name={state.persons[1].name} age={state.persons[1].age} />
-      <Person name={state.persons[2].name} age={state.persons[2].age}>
-        My hobbies: Racing
-      </Person>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi I am react app</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          My hobbies: Racing
+        </Person>
+      </div>
+    );
+  }
 }
 
 export default App;
