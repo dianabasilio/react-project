af0b38b8d2b347bab38f99cf45f7f2eddb603fe9
dianabasilio <dianabasilio99@gmail.com>
2022-08-14 17:59:27 -0500

    New way of manipulating the State.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, {useState} from 'react'
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+function App() {
+  const [ personState, setPersonState ] = useState({
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 70 },
       { name: "Soph", age: 17 },
     ],
-    otherState: "some other value",
-  };
+    otherState: 'some other value'
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () =>{
     //Do not do this!!! : state.persons[0].name = 'Maximun';
-    this.setState({
-      persons: [
-        { name: "Maximiliano", age: 28 },
-        { name: "Manuel", age: 20 },
-        { name: "Sophie", age: 17 },
-      ],
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1> Hi I am react app</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
-          My hobbies: Racing
-        </Person>
-      </div>
-    );
+    setPersonState({persons: [
+      { name: "Maximiliano", age: 28 },
+      { name: "Manuel", age: 20 },
+      { name: "Sophie", age: 17 },
+    ] })
   }
+
+  return (
+    <div className="App">
+      <h1> Hi I am react app</h1>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+      <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+      <Person name={personState.persons[2].name} age={personState.persons[2].age}>
+        My hobbies: Racing
+      </Person>
+    </div>
+  );
 }
 
 export default App;
