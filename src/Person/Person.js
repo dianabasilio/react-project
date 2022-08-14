02ed2a38145c0a3bf6264adf16141dedcb49afc2
dianabasilio <dianabasilio99@gmail.com>
2022-08-14 18:43:19 -0500

    Adding styles.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import './Person.css'
 
 const Person = (props) =>{
     //this jsx sintax is transform to react create element, to call this method we need
     //to import react
     return (
-      <div>
+      <div className="Person">
         <p onClick={props.click}>
           I'm {props.name} and I am years {props.age} old!
         </p>
         <p>
             {props.children}
         </p>
-        <input type="text" onChange={props.changed} />
+        <input type="text" onChange={props.changed} value={props.name} />
       </div>
     );
 }
