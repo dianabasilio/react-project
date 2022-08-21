be754ce213462a68e4aca4d59a7871526d0f7275
dianabasilio <dianabasilio99@gmail.com>
2022-08-21 17:57:32 -0500

    Map people.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import React, { useState } from "react";
 import Person from "./Person/Person";
 
 function App() {
-  const [personState, setPersonState] = useState({
-    people: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 70 },
-      { name: "Soph", age: 17 },
-    ],
-  });
+  const [personState, setPersonState] = useState([
+    { name: "Max", age: 28 },
+    { name: "Manu", age: 70 },
+    { name: "Soph", age: 17 },
+  ]);
 
   const [showPerson, setShowPerson] = useState(false);
 
   const togglePersonsHandler = (newName) => {
     //Do not do this!!! : state.people[0].name = 'Maximun';
-    setPersonState({
-      people: [
-        { name: newName, age: 28 },
-        { name: "Manuel", age: 20 },
-        { name: "Sophie", age: 17 },
-      ],
-    });
+    setPersonState([
+      { name: newName, age: 28 },
+      { name: "Manuel", age: 20 },
+      { name: "Sophie", age: 17 },
+    ]);
     setShowPerson(!showPerson);
   };
 
@@ -48,22 +44,9 @@ function App() {
   if (showPerson) {
     people = (
       <div>
-        <Person
-          name={personState.people[0].name}
-          age={personState.people[0].age}
-        />
-        <Person
-          name={personState.people[1].name}
-          age={personState.people[1].age}
-          click={() => togglePersonsHandler("Max!")}
-          changed={nameChangeHandler}
-        />
-        <Person
-          name={personState.people[2].name}
-          age={personState.people[2].age}
-        >
-          My hobbies: Racing
-        </Person>
+        {personState.map((person) => {
+          return <Person name={person.name} age={person.age} />;
+        })}
       </div>
     );
   }
